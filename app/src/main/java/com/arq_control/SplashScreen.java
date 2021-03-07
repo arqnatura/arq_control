9bf5114bcb86de82000cc98c5fdfa38648d9e4c0
ANGEL ESTEVEZ <=>
2021-03-07 19:01:08 +0000

    Tercer commit. Frontend_01

--- app/src/main/java/com/arq_control/SplashScreen.java
+++ app/src/main/java/com/arq_control/SplashScreen.java
@@ -7,6 +7,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
+import com.arq_control.ui.login.LoginActivity;
+
 public class SplashScreen extends Activity {
 
     @Override
@@ -17,10 +19,10 @@ public class SplashScreen extends Activity {
         new Handler().postDelayed(new Runnable(){
             @Override
             public void run() {
-                Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                Intent intent = new Intent(SplashScreen.this, LoginActivity.class);
                 startActivity(intent);
             }
-        },4000);
+        },3000);
 
     }
 }
